ef8fa118e219e4ad5a92015547b829df726ead16
brixi <denikucevic@gmail.com>
2023-01-21 20:27:47 +0100

    hiding 🔑s

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,8 @@ import { createClient } from "@supabase/supabase-js";
 import { Provider } from "./context";
 
 const client = createClient(
-  "https://xqplcuckdjktipxgcpvk.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhxcGxjdWNrZGprdGlweGdjcHZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzI3NDM1NjUsImV4cCI6MTk4ODMxOTU2NX0.IJl_-b12tkqx-tp8Wah801cWrzas9y9_8hNaG058QQw"
+  import.meta.env.VITE_REACT_APP_SUPABASE_URL ?? "",
+  import.meta.env.VITE_REACT_APP_SUPABASE_ANON_KEY ?? ""
 );
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
